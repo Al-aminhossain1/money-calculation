3f1ec210c3c68fc9da9185d4fba9446476dcd2b4
Al-aminhossai1 <md.al.aminhossain1785@gmail.com>
2022-02-17 23:38:18 +0600

    remaining balance update

--- js/calculate.js
+++ js/calculate.js
@@ -1,12 +1,9 @@
 document.getElementById('calculate').addEventListener('click', function () {
     const foodInput = document.getElementById('food-input');
     const foodAmount = parseFloat(foodInput.value);
-    console.log(foodAmount);
     // Rent Calculation part
     const rentInput = document.getElementById('rent-input');
     const rentAmount = parseFloat(rentInput.value);
-    console.log(rentAmount);
-
     // Cloths Calculations part
     const clothInput = document.getElementById('cloth-input');
     const clothAmount = parseFloat(clothInput.value);
@@ -14,7 +11,6 @@ document.getElementById('calculate').addEventListener('click', function () {
 
     // previous Expenses 
     const previousExpenses = foodAmount + rentAmount + clothAmount;
-    // console.log(previousExpenses);
     //  Total Expenses Calculation
     const totalcost = document.getElementById('total-expenses');
     const totalcostText = totalcost.innerText
@@ -27,12 +23,11 @@ document.getElementById('calculate').addEventListener('click', function () {
 
 
     // save balance
-    let balanceInput = document.getElementById('total-balance');
+    const balanceInput = document.getElementById('total-balance');
     const balanceInputText = balanceInput.innerText
     const balanceAmount = parseFloat(balanceInputText);
     const balance = incomeAmount - previousExpenses;
     balanceInput.innerText = balance;
-    // console.log(balance);
 });
 document.getElementById('save-but').addEventListener('click', function () {
     const saveInput = document.getElementById('save-input');
@@ -44,7 +39,37 @@ document.getElementById('save-but').addEventListener('click', function () {
     // updete Saving Amount
     const savingAmountText = document.getElementById('Saving-Amount');
     const savingAmount = parseFloat(savingAmountText.innerText);
-    const savingTotalAmount = incomeAmount / saveInputAmount;
-    console.log(savingTotalAmount);
+    const savingTotalAmount = incomeAmount * saveInputAmount / 100;
     savingAmountText.innerText = savingTotalAmount;
+
+    // first function theke copy kora
+
+    const foodInput = document.getElementById('food-input');
+    const foodAmount = parseFloat(foodInput.value);
+    // Rent Calculation part
+    const rentInput = document.getElementById('rent-input');
+    const rentAmount = parseFloat(rentInput.value);
+    // Cloths Calculations part
+    const clothInput = document.getElementById('cloth-input');
+    const clothAmount = parseFloat(clothInput.value);
+    console.log(clothAmount);
+
+    // previous Expenses 
+    const previousExpenses = foodAmount + rentAmount + clothAmount;
+
+
+    // // save balance
+    const balanceInput = document.getElementById('total-balance');
+    const balanceInputText = balanceInput.innerText
+    const balanceAmount = parseFloat(balanceInputText);
+    const balance = incomeAmount - previousExpenses;
+    balanceInput.innerText = balance;
+    // Remaining Balanace
+    const remainingBalanaceText = document.getElementById('Remaining-Balanace');
+    const remainingBalanace = parseFloat(remainingBalanaceText.innerText);
+    const remainingBalanaceTotal = balance - savingTotalAmount;
+    console.log(remainingBalanaceTotal);
+    remainingBalanaceText.innerText = remainingBalanaceTotal;
+
+
 })
