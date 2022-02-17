bfc533c52aca8ec3be956226f0e3882f83a881fa
Al-aminhossai1 <md.al.aminhossain1785@gmail.com>
2022-02-17 21:38:56 +0600

    eroor solv

--- js/calculate.js
+++ js/calculate.js
@@ -1,15 +1,20 @@
 document.getElementById('calculate').addEventListener('click', function () {
     const foodInput = document.getElementById('food-input');
     const foodAmount = parseFloat(foodInput.value);
-
+    console.log(foodAmount);
     // Rent Calculation part
     const rentInput = document.getElementById('rent-input');
     const rentAmount = parseFloat(rentInput.value);
+    console.log(rentAmount);
+
     // Cloths Calculations part
     const clothInput = document.getElementById('cloth-input');
-    const clothAmount = parseFloat(rentInput.value);
+    const clothAmount = parseFloat(clothInput.value);
+    console.log(clothAmount);
+
     // previous Expenses 
     const previousExpenses = foodAmount + rentAmount + clothAmount;
+    // console.log(previousExpenses);
     //  Total Expenses Calculation
     const totalcost = document.getElementById('total-expenses');
     const totalcostText = totalcost.innerText
@@ -27,5 +32,5 @@ document.getElementById('calculate').addEventListener('click', function () {
     const balanceAmount = parseFloat(balanceInputText);
     const balance = incomeAmount - previousExpenses;
     balanceInput.innerText = balance;
-    console.log(balance);
+    // console.log(balance);
 });
